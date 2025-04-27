c4a9d40b6c337efb56a8e4e045367ca0804db090
Alexandre Magnier <exaland@gmail.com>
2025-04-27 15:31:48 +0000

    Initial Commit

---
+++ index.js
@@ -0,0 +1,73 @@
+import { S3Client, PutObjectCommand } from "@aws-sdk/client-s3";
+import dotenv from "dotenv";
+dotenv.config();
+import fs from "fs";
+import { exec } from "child_process";
+
+const s3Client = new S3Client({
+    region: process.env.AWS_REGION,
+    credentials: {
+        accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+        secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+    },
+});
+
+const bucketName = process.env.AWS_BUCKET_NAME;
+const backupFileName = `backup_your_project_${Date.now()}.sql`;
+const backupFilePath = process.env.BACKUP_FILE_PATH;
+const backupFileKey = `${backupFileName}`;
+
+const mysqlDump = async () => {
+    return new Promise((resolve, reject) => {
+        exec(
+            `mysqldump -u ${process.env.DB_USER} -p'${process.env.DB_PASSWORD}' ${process.env.DB_NAME} > ${backupFileName}`,
+            (error, stdout, stderr) => {
+                if (error) {
+                    console.error(`Error executing mysqldump: ${error}`);
+                    reject(error);
+                } else {
+                    console.log(`mysqldump output: ${stdout}`);
+                    resolve(stdout);
+                }
+            }
+        );
+    });
+};
+
+const uploadBackupToS3 = async () => {
+    try {
+        const fileStream = fs.createReadStream(backupFileName);
+
+        const readFile = fs.readFileSync(backupFileName);
+        if (!readFile) {
+            console.error("Error reading the backup file.");
+            return;
+        }
+
+        console.log(`Uploading backup file: ${backupFileName} to S3 bucket: ${bucketName}`);    
+        const uploadParams = {
+            Bucket: bucketName,
+            Key: `backups/${backupFileName}`,
+            Body: readFile,   
+        };
+
+        const command = new PutObjectCommand(uploadParams);
+        const data = await s3Client.send(command);  
+
+        console.log(`Backup uploaded successfully: ${JSON.stringify(data)}`);
+    } catch (error) {
+        console.error("Error uploading backup to S3:", error);
+    }
+};
+
+const runBackupProcess = async () => {
+    try {
+        await mysqlDump();
+        await uploadBackupToS3();
+        console.log("Backup process completed successfully.");
+    } catch (error) {
+        console.error("Error during backup process:", error);
+    }
+};
+
+runBackupProcess();
